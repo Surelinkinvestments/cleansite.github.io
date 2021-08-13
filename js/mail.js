7026aa0b1ece5c30c92c6a0625a203cd09a7ddcb
jod35 <jodestrevin@gmail.com>
2021-08-13 15:29:38 +0300

    implement ajax

---
+++ js/mail.js
@@ -0,0 +1,26 @@
+const form=document.querySelector('form');
+
+
+form.addEventListener('submit',(e)=>{
+    e.preventDefault()
+    console.log("form has  been submitted")
+
+
+    let form_data=new FormData(form);
+
+    const body={email:form_data.get('email'),message:form_data.get('subject')}
+
+    console.log(body)
+
+    const requestOptions={
+        method: 'POST',
+        headers:{
+            'Accept': 'application/json'
+        },
+        body:JSON.stringify(body)
+    }
+
+    fetch('https://formspree.io/f/xoqykyvr',requestOptions)
+    .then(res=>res.json())
+    .then(data=>console.log(data));
+})
