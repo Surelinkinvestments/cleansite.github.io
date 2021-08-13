2a4e884896a9a207306436bda9054638c0ae1af1
jod35 <jodestrevin@gmail.com>
2021-08-13 14:33:02 +0300

    send request to clean via ajax forms

--- js/quotation.js
+++ js/quotation.js
@@ -1,1 +1,38 @@
-https://formspree.io/f/xgerpvlg
+const form=document.querySelector('form');
+const messageInput=document.querySelector('.message-input');
+
+
+form.addEventListener('submit',(e)=>{
+    e.preventDefault()
+
+
+    let form_data=new FormData(form);
+
+    const body={email:form_data.get('email')
+                ,message:messageInput.value,
+                phone:form_data.get('phone'),
+                beds:form_data.get('beds'),
+                baths:form_data.get('baths'),
+
+                
+            }
+
+    console.log(body)
+
+    const requestOptions={
+        method: 'POST',
+        headers:{
+            'Accept': 'application/json'
+        },
+        body:JSON.stringify(body)
+    }
+
+    fetch('https://formspree.io/f/xgerpvlg',requestOptions)
+    .then(res=>res.json())
+    .then(data=>{console.log(data)
+        form.reset();
+        alert("Email sent successfully")
+    });
+})
+
+// action=""
