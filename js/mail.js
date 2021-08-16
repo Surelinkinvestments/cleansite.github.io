f3a35b7542f702215867df6b72f7f1e64ea4eab3
jod35jon <88432403+jod35jon@users.noreply.github.com>
2021-08-16 13:43:20 +0300

    add  email subject

--- js/mail.js
+++ js/mail.js
@@ -1,39 +1,43 @@
-const form=document.querySelector('form');
-const modalContainer=document.querySelector('.modal-container');
-const closeButton=document.querySelector('.close-btn');
+const form = document.querySelector('form');
+const modalContainer = document.querySelector('.modal-container');
+const closeButton = document.querySelector('.close-btn');
 
 
-form.addEventListener('submit',(e)=>{
+form.addEventListener('submit', (e) => {
     e.preventDefault()
     console.log("form has  been submitted")
 
 
-    let form_data=new FormData(form);
+    let form_data = new FormData(form);
 
-    const body={email:form_data.get('email'),message:form_data.get('message')}
+    const body = {
+        email: form_data.get('email'),
+        message: form_data.get('message'),
+        subject: form_data.get('subject')
+    }
 
     console.log(body)
 
-    const requestOptions={
+    const requestOptions = {
         method: 'POST',
-        headers:{
+        headers: {
             'Accept': 'application/json'
         },
-        body:JSON.stringify(body)
+        body: JSON.stringify(body)
     }
 
-    fetch('https://formspree.io/f/mnqlonbv',requestOptions)
-    .then(res=>res.json())
-    .then(data=>{
-        console.log(data)
-        form.reset();
-        modalContainer.style.display='block';
-    });
+    fetch('https://formspree.io/f/mnqlonbv', requestOptions)
+        .then(res => res.json())
+        .then(data => {
+            console.log(data)
+            form.reset();
+            modalContainer.style.display = 'block';
+        });
 })
 
 
 // action=""
 
-closeButton.addEventListener('click',()=>{
-    modalContainer.style.display='none'
+closeButton.addEventListener('click', () => {
+    modalContainer.style.display = 'none'
 })
